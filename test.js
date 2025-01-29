326ce3d0693ed6812385a23fcd1a97e993204eb3
Aslan3876 <arasalanshaikh317@gmail.com>
2025-01-29 19:35:33 +0000

    datatypes

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
+"use strict";
+
 console.log("Hi");
+let a = "Apple";
+// const b = "Ball";
+// var c  = "Code";
+
+// console.table([a,b,c]);
+
+//Datatypes..
+//alert is treated differently in node js and in browser..
+//undefined null bolean bigint string number bigint.
+//symbol
+
+//object 
+//typeof
+console.log(typeof a);
